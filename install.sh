84bf1478387acea3dcc6d49db8f97c7cc2c33365
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 12:19:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,26 +2,28 @@
 
 apt_update(){
 	echo "Update system. Please wait."
-	apt-get update
+	apt-get update > /dev/null 2>&1
 }
 utilit_install(){
 	echo "Installation utils. Please wait."
 	
 	echo "Installation php. Please wait."
-	apt-get -y install  ca-certificates apt-transport-https software-properties-common
+	{
+	apt-get -y install  ca-certificates apt-transport-https software-properties-common 
 	add-apt-repository -y ppa:ondrej/php
-	
+	} > /dev/null 2>&1
 	echo "Installation docksal. Please wait."
+	{
 	curl -o docksal.sh -fsSL https://get.docksal.io
 	sh docksal.sh > log.txt
-	
+	} > /dev/null 2>&1
 	echo "Installation phpstorm. Please wait."
-	snap install phpstorm --classic
+	snap install phpstorm --classic > /dev/null 2>&1
 }
 
 # update all packages
-apt_update
-utilit_install
+apt_update > /dev/null 2>&1
+utilit_install > /dev/null 2>&1
 
 echo "Drupal Auto installation is completed."
 sleep 5
