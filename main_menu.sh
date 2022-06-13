23a713b87e0375679c08f48a2b453bfebdec9511
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 13:11:51 +0300

    Update main_menu.sh

--- main_menu.sh
+++ main_menu.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-echo "select the operation ************"
-echo "  1)operation 1"
-echo "  2)operation 2"
-echo "  3)operation 3"
-echo "  4)operation 4" 
+echo "  Select the operation"
+echo "  1) Create new project"
+echo "  2) Deploy an existing project"
+echo "  3) Display a list of projects"
+echo "  4) Exit" 
 
 read n
 case $n in
   1) echo "You chose Option 1";;
   2) echo "You chose Option 2";;
   3) echo "You chose Option 3";;
-  4) echo "You chose Option 4";;
+  4) echo "You chose Option 4"
+  break
   *) echo "invalid option";;
 esac
