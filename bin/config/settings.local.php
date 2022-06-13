2287724048e0847019aa28c9cf359d65a3860ff6
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 14:23:12 +0300

    Create settings.local.php

---
+++ bin/config/settings.local.php
@@ -0,0 +1,14 @@
+$databases['default']['default'] = [
+  'database' => 'default',
+  'username' => 'user',
+  'password' => 'user',
+  'host' => 'db',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+];
+
+$settings['hash_salt'] = '123';
+$settings["config_sync_directory"] = "../config/sync";
+$settings['file_private_path'] = 'sites/default/files/';
