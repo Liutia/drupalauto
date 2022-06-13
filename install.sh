fa359a71fef1fba99104321ea1f956035d5fb693
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 11:17:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 apt_update(){
-	print "Update system. Please wait." 1
-	apt-get  update
+	print "Update system. Please wait."
+	apt-get update
 }
 utilit_install(){
-	print "Installation utils. Please wait." 1
+	print "Installation utils. Please wait."
 	apt-get -y install  ca-certificates apt-transport-https software-properties-common
-  add-apt-repository ppa:ondrej/php
-  curl -fsSL https://get.docksal.io
+	add-apt-repository -y ppa:ondrej/php
+	curl -fsSL https://get.docksal.io
 }
 
 # update all packages
 apt_update
 utilit_install
 
-print "Drupal Auto installation is completed." 1
+print "Drupal Auto installation is completed."
 sleep 5
 drupalauto_menu.sh
