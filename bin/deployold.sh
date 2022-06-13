c768d8d4ac5e15ce91406b4ffb76858e76242f0e
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 14:00:54 +0300

    Update deployold.sh

--- bin/deployold.sh
+++ bin/deployold.sh
@@ -5,15 +5,40 @@ read path
 echo "Write project repository path"
 read repo
 echo "Write project directory name"
-echo "Default repository name"
+# echo "Default name - repository name"
 read name
+echo "Write path to db"
+read db_path
 
-# cd $path
-if [ -z $name ]
-then
-echo "0"
-else
-echo "1"
-fi
-# cd drupalauto/
-sh main_menu.sh
+cd $path
+git clone $repo $name
+cd $name
+
+# if [ -z $name ]
+# then
+
+# else
+# echo "1"
+# fi
+echo "Configuration site. Please wait"
+{
+fin up
+fin composer install
+fin config set --env=local XDEBUG_ENABLED=1
+fin addon install pma
+fin project start
+fin db import $db_path
+fin drush cr
+
+fin 
+# if [ -z  ]
+# then
+
+# else
+# echo "1"
+# fi
+
+} > /dev/null 2>&1
+echo "Local site created."
+pwd
+break
