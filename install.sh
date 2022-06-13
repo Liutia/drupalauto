ad7641d04849281251b043d9552265fd731447a8
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 09:44:10 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt_update(){
+	print "Update system. Please wait." 1
+	apt-get  update
+}
+utilit_install(){
+	print "Installation utils. Please wait." 1
+	apt-get -y install  ca-certificates apt-transport-https software-properties-common
+  add-apt-repository ppa:ondrej/php
+  bash <(curl -fsSL https://get.docksal.io)
+}
+
+# update all packages
+apt_update
+utilit_install
+
+print "Drupal Auto installation is completed." 1
+sleep 5
+drupalauto_menu.sh
