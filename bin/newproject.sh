969fb862f2560e53517ea70a803e8dd84b7ae002
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 13:29:46 +0300

    Update newproject.sh

--- bin/newproject.sh
+++ bin/newproject.sh
@@ -4,5 +4,12 @@ echo "Write path to project directory"
 read path
 cd $path
 fin p create
+
+{
+fin config set --env=local XDEBUG_ENABLED=1
+fin addon install pma
+fin project start
+} > /dev/null 2>&1
+
 cd drupalauto/
 sh main_menu.sh
