069ba6318787cd02fd2039c52e09e36c63ec1987
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 11:30:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# common subs
-print(){
-    msg=$1
-    notice=${2:-0}
-    [[ ( $SILENT -eq 0 ) && ( $notice -eq 1 ) ]] && echo -e "${msg}"
-    [[ ( $SILENT -eq 0 ) && ( $notice -eq 2 ) ]] && echo -e "\e[1;31m${msg}\e[0m"
-    echo "$(date +"%FT%H:%M:%S"): $$ : $msg" >> $LOG
-}
-
 apt_update(){
-	print "Update system. Please wait." 1 1
+	echo "Update system. Please wait."
 	apt-get update
 }
 utilit_install(){
-	print "Installation utils. Please wait." 1
+	echo "Installation utils. Please wait."
 	apt-get -y install  ca-certificates apt-transport-https software-properties-common
 	add-apt-repository -y ppa:ondrej/php
 	wget --no-check-certificate https://get.docksal.io
@@ -24,6 +15,6 @@ utilit_install(){
 apt_update
 utilit_install
 
-print "Drupal Auto installation is completed." 1
+echo "Drupal Auto installation is completed."
 sleep 5
 drupalauto_menu.sh
