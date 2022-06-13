96a7081b3f3be2de977690d1ae9f1f1f10615d62
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 13:27:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,27 +6,17 @@ apt_update(){
 }
 utilit_install(){
 	echo "Installation utils. Please wait."
-	if dpkg -s ca-certificates apt-transport-https software-properties-common > /dev/null 2>&1
-	then echo "Php already installed."
-	else
 	echo "Installation php. Please wait."
 	{
 	apt-get -y install  ca-certificates apt-transport-https software-properties-common 
 	add-apt-repository -y ppa:ondrej/php
 	} > /dev/null 2>&1
-	fi
-	if fin > /dev/null 2>&1
-	then echo "Docksal already installed."
-	else
+
 	echo "Installation docksal. Please wait."
 	bash <(curl -fsSL https://get.docksal.io) > /dev/null 2>&1
-	fi
-	if snap info phpstorm > /dev/null 2>&1
-	then echo "Phpstorm already installed."
-	else
+
 	echo "Installation phpstorm. Please wait."
 	snap install phpstorm --classic > /dev/null 2>&1
-	fi
 }
 
 # update all packages
