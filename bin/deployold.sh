bddcbda9ce661403ff454dd99e550b55eb1ef055
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 15:03:05 +0300

    Update deployold.sh

--- bin/deployold.sh
+++ bin/deployold.sh
@@ -37,13 +37,11 @@ else
 cd web/sites/default/
 fi
 
-touch settings.php
 echo "if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {" >> settings.php
 echo "include $app_root . '/' . $site_path . '/settings.local.php';" >> settings.php
 echo "}" >> settings.php
 
 wget -P . "https://github.com/Liutia/drupalauto/blob/main/bin/config/settings.local.php"
-
 } > /dev/null 2>&1
 
 echo "Local site created."
