067d9a0a376d2a4287bc513240ff4c5efaf74eb8
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-13 12:19:06 +0300

    Update drupalauto.sh

--- drupalauto.sh
+++ drupalauto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd
-rm -rf drupalauto/
-git clone https://github.com/Liutia/drupalauto.git
+rm -rf drupalauto/ > /dev/null 2>&1
+git clone https://github.com/Liutia/drupalauto.git > /dev/null 2>&1
 
 cd drupalauto/
 sh install.sh
