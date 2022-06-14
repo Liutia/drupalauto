f6ed5283ac1d61d0f818450fc0af2fdf60bcbc60
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-14 12:49:56 +0300

    Update drupalauto.sh

--- drupalauto.sh
+++ drupalauto.sh
@@ -5,4 +5,14 @@ rm -rf drupalauto/ > /dev/null 2>&1
 git clone https://github.com/Liutia/drupalauto.git > /dev/null 2>&1
 
 cd drupalauto/
+echo "Install Utility?[yes,no]"
+echo "  "
+read n
+echo "  "
+
+if [ $n = yes ]
+then
 bash install.sh
+else
+bash drupalauto_menu.sh
+fi
