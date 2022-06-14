a14206479941fef95407cf696a933eab11a00a90
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-14 12:56:37 +0300

    Update drupalauto.sh

--- drupalauto.sh
+++ drupalauto.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 cd
-rm -rf drupalauto/ > /dev/null 2>&1
-git clone https://github.com/Liutia/drupalauto.git > /dev/null 2>&1
-
-cd drupalauto/
+if find . -type d -name "drupalauto"
+then
 echo "  "
-echo "  Install Utility?[yes,no]"
+echo "  Select the option"
+echo "  1) Update script"
+echo "  2) Install/Update Utility"
+echo "  3) Continue"
 echo "  "
+cd 
 read n
 echo "  "
-
-if [ $n = yes ]
-then
-bash install.sh
+case $n in
+  1) rm -rf drupalauto/ > /dev/null 2>&1 ; git clone https://github.com/Liutia/drupalauto.git > /dev/null 2>&1;;
+  2) bash drupalauto/install.sh;;
+  3) bash drupalauto/drupalauto_menu.sh ;;
+  *) echo "Invalid option";;
+esac
 else
-bash drupalauto_menu.sh
+rm -rf drupalauto/ > /dev/null 2>&1
+git clone https://github.com/Liutia/drupalauto.git > /dev/null 2>&1
+bash drupalauto/drupalauto.sh
 fi
