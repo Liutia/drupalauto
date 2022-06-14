f6e76810b7fd8c1f4bed338a04791c0e2438b19b
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-14 11:41:13 +0300

    Update newproject.sh

--- bin/newproject.sh
+++ bin/newproject.sh
@@ -5,15 +5,10 @@ read path
 cd $path
 fin p create
 
-{
-fin config set --env=local XDEBUG_ENABLED=1
-fin addon install pma
-fin project start
-} > /dev/null 2>&1
+bash ~/drupalauto/bin/addons.sh
 
 echo "Local site created."
 pwd
 
 cd
-cd drupalauto/
-bash main_menu.sh
+bash drupalauto/main_menu.sh
