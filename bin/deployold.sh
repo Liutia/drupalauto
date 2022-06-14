3c4869f72630197c1445ab0b51fbdba6e674c632
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-14 12:44:04 +0300

    Update deployold.sh

--- bin/deployold.sh
+++ bin/deployold.sh
@@ -14,25 +14,25 @@ cd $path
 git clone $repo $name > /dev/null 2>&1
 cd $name
 
-# echo "Configuration site. Please wait"
+echo "Configuration site. Please wait"
 
-# fin up > /dev/null 2>&1
+fin up > /dev/null 2>&1
 
-# echo "Set composer version[1,2]"
-# read V
-# case $V in
-#   1) fin config set COMPOSER_DEFAULT_VERSION=1 > /dev/null 2>&1;;
-#   2) fin config set COMPOSER_DEFAULT_VERSION=2 > /dev/null 2>&1;;
-#   *) echo "Invalid version";;
-# esac
+echo "Set composer version[1,2]"
+read V
+case $V in
+  1) fin config set COMPOSER_DEFAULT_VERSION=1 > /dev/null 2>&1;;
+  2) fin config set COMPOSER_DEFAULT_VERSION=2 > /dev/null 2>&1;;
+  *) echo "Invalid version";;
+esac
 
-# echo "Your composer version $V"
-
-# fin restart > /dev/null 2>&1
-# fin composer install
-# bash ~/drupalauto/bin/addons.sh
-# fin db import $db_path
-# fin drush cr
+echo "Your composer version $V"
+{
+fin restart
+fin composer install
+bash ~/drupalauto/bin/addons.sh
+fin db import $db_path
+fin drush cr
 
 if find . -type d -name "web" 
 then
@@ -43,8 +43,8 @@ echo "if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {in
 cp ~/drupalauto/bin/config/settings.local.php sites/default/
 fi
 
-# } > /dev/null 2>&1
+} > /dev/null 2>&1
 
 echo "Local site created."
 pwd
-break
+bash drupalauto/main_menu.sh
