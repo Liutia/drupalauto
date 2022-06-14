4e3c7f43835152ae4eb9e811bebe8bfba9ef9b24
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-14 11:36:05 +0300

    Update main_menu.sh

--- main_menu.sh
+++ main_menu.sh
@@ -7,11 +7,11 @@ echo "  2) Deploy an existing project"
 echo "  3) Display a list of projects"
 echo "  4) Exit" 
 echo "  "
-
+cd 
 read n
 case $n in
-  1) bash bin/newproject.sh;;
-  2) bash bin/deployold.sh;;
+  1) bash drupalauto/bin/newproject.sh;;
+  2) bash drupalauto/bin/deployold.sh;;
   3) fin pl -a ; bash main_menu.sh ;;
   4) break;;
   *) echo "invalid option";;
