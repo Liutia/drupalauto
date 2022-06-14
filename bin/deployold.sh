2dd85c40992e7c180f32e53622d9a95a490595ff
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-14 12:38:43 +0300

    Update deployold.sh

--- bin/deployold.sh
+++ bin/deployold.sh
@@ -37,7 +37,7 @@ cd $name
 if find . -type d -name "web" 
 then
 echo "if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {include $app_root . '/' . $site_path . '/settings.local.php';}" >> sites/default/settings.php
-cp ~/drupalauto/bin/config/settings.local.php web/sites/default/
+cp ~/drupalauto/bin/config/settings.local.php sites/default/
 else
 echo "if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {include $app_root . '/' . $site_path . '/settings.local.php';}" >> web/sites/default/settings.php
 cp ~/drupalauto/bin/config/settings.local.php web/sites/default/
