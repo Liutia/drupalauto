55bf62d4036cc9f5af3918dec864beff7b84395b
Liutia <69680673+Liutia@users.noreply.github.com>
2022-06-15 10:35:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ utilit_install(){
 	{
 	apt-get -y install  ca-certificates apt-transport-https software-properties-common 
 	add-apt-repository -y ppa:ondrej/php
-# 	bash drupalauto/utility/docksal.sh
+	bash <(curl -fsSL https://get.docksal.io)
 	snap install phpstorm --classic
 	} > /dev/null 2>&1
 }
